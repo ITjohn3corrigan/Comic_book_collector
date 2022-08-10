a0c1739296ec4790f077f580e29b520fde11386d
john <john3corrigan@gmail.com>
2022-08-09 20:43:37 -0400

    Initial Commit

---
+++ Comic_book_collector/Comic_book_collectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Comic_book_collectorApp.swift
+//  Comic_book_collector
+//
+//  Created by John Corrigan on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Comic_book_collectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
